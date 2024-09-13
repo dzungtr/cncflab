d0f45fa7fb9b5d1a3d7625c6e671eb0f21a3ab93
dzungtr <dzung.tr@outlook.com>
2024-09-13 22:14:24 +1000

    chore: improve Hotrod installation script

--- usecases/hotrod/install.sh
+++ usecases/hotrod/install.sh
@@ -1,4 +1,4 @@
-export
+export SIGNOZ_NAMESPACE=observability
 
 curl -sL https://github.com/SigNoz/signoz/raw/develop/sample-apps/hotrod/hotrod-install.sh \
   | HELM_RELEASE=signoz-obs SIGNOZ_NAMESPACE=observability bash
